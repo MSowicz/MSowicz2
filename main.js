eed73ad84fc2992502f434170a57c412d3db1ef0
MSowicz <44524028+MSowicz@users.noreply.github.com>
2018-12-01 19:05:24 +0100

    01. 12.2018 JAVASCRIPT

--- main.js
+++ main.js
@@ -1,119 +1,73 @@
-const $field = document.getElementById('field');
-const $btn = document.getElementById('btn');
-const $btn2 = document.getElementById('btn2');
-const $btn3 = document.getElementById('btn3');
-const $btn4 = document.getElementById('btn4');
-const $btn5 = document.getElementById('btn4');
+const newP = document.createElement('p');
+const newPText = document.createTextNode('Przykładowy tekst');
+newP.appendChild(newPText);
+newP.setAttribute('id','test');
 
-const MyArray = [1, 3, 4, 2, 5,8,0];
+const parentListP = document.getElementById('parent-element');
+parentListP.appendChild(newP);
 
-$btn.addEventListener('click', function () {
-    const valueFromField = $field.value;
-    updateMyArray(valueFromField);
+let tab=['a','b', 'c'];
+for (let i=0; i<tab.length; i++){
+    const newP= document.createElement('P');
+    const newPText = document.createTextNode(`Przykładowy ${1+tab[i]}`);
+    newP.appendChild(newPText);
+    newP.setAttribute('id',`test- ${i+1}`);
+    parentListP.appendChild(newP);
+}
+/*
+for (let i=0; i<3; i++){
     
-})
-$btn2.addEventListener('click', function () {
-    const valueFromField = $field.value;
-    updateMyArray2(valueFromField);
-   
-})
+const newPText = document.createTextNode(`Przykładowy ${1+i}`);
+const newTexth2 = document.createTextNode(`${1+i}`); 
+//tworzenie nowego h2
+const newh2=document.createElement("h2");
+//wrzucanie tekstu do h2
+newh2=appendChild(newTexth2);
+parentListP.appendChild('newh2');
+}
+//tworzenie nowego p
+const newh2=document.createElement("p");
+//wrzucanie tekstu do p
+newP=appendChild(newTextP);
+parentListP.appendChild('newP');
 
-$btn3.addEventListener('click', function () {
-    const valueFromField = $field.value;
-    updateMyArray3(valueFromField);
-   
-})
-
-$btn4.addEventListener('click', function () {
-    const valueFromField = $field.value;
-    updateMyArray4(valueFromField);
-   
-})
-
-$btn5.addEventListener('click', function () {
-    const valueFromField = $field.value;
-    updateMyArray5(valueFromField);
-   
-})
-
-function updateMyArray(value) {
-    const $result = document.getElementById('result');
-    value = parseFloat(value);
-    if (isNaN(value)) {
-        value = Math.floor(Math.random() * 20) + 1;
+/*
+newh2.onclick=function(){
+    if (newP.classList.contains('hide'){
+        newP.classList.add('newTextP');
+    }       
+           
+    else 
+    newP.classList.remove('newTextP');
+} */
+// biblioteka do obługi DOM umrellajs
+document.forms.myform.onsubmit= function() {
+    if (this.nazwa.value){
+        newP=appendChild(this.nazwa.value);
+       parentListP.appendChild('newP');
+      //  var li=document.createElement("LI");
+      //  li.innerHTML=this.nazwa.value;
+       // document.getElementById("lista").appendChild(li);
+        this.nazwa.value= "";
+    this.nazwa.focus();
+      //  li.onclick=usuń;
     }
-    
-    MyArray.push(value); 
-    $result.textContent=MyArray;
-    showMessage(value, MyArray.length);
-    
+    return false;
 }
 
-function updateMyArray2(value) {
-    const $result = document.getElementById('result');
-    value = parseFloat(value);
-    if (isNaN(value)) {
-        value = Math.floor(Math.random() * 20) + 1;
-    }
-    
-    MyArray.unshift(value); 
-    $result.textContent=MyArray;
-    showMessage(value);
-}
+//const newa=document.createElement("a");
+//const newTexta = document.createTextNode("usun"); 
+//newa=appendChild(newTexta);
+//parentListP.appendChild(newa);
+//newa.onclick= function(){
+  //  newa.parentNode.removeChild(this);
 
-function updateMyArray3(value) {
-    const $result = document.getElementById('result');
-    val = MyArray.pop(); 
-    $field.value=val;
-    
-    
-    $result.textContent=MyArray;
-    
-}
 
-function updateMyArray4(val) {
-    const $result = document.getElementById('result');
-    val = MyArray.shift(); 
-    $field.value=val;
-    
-    
-    $result.textContent=MyArray;
-    
-}
 
-function updateMyArray5(value) {
-    const $result = document.getElementById('result');
-    value = parseFloat(value);
-    if (isNaN(value)) {
-        value = Math.floor(Math.random() * 20) + 1;
-    }
-    let val=Math.floor(Math.random() * 5) + 1;
-     MyArray[val]=value;
-    $result.textContent=MyArray;
-    //showMessage(value);
-}
+newP=onmouseout = function()
+{this.styleMedia.bacgraund = 'red';}
 
-function updateMyArray6(value) {
-    const $result = document.getElementById('result');
-    value = parseFloat(value);
-    if (isNaN(value)) {
-        value = Math.floor(Math.random() * 20) + 1;
-    }
-    
-    MyArray.splice()
-   ; 
-    $result.textContent=MyArray;
-    
-}
+  
 
-function showMessage(value, index) {
-    const $message = document.getElementById('message');
-    const textMsg = `dodano wartosc ${value} na pozycji ${index} `;
-    $message.textContent = textMsg;
-}
 
-function showMessage2(value) {
-    const $message = document.getElementById('message');
-    const textMsg = `dodano wartosc ${value} na pozycji 1`;
-    $message.textContent = textMsg;
-}
+
