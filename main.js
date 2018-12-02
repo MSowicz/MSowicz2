959c231db66ae5b738a65114018e36eac939c1e5
MSowicz <44524028+MSowicz@users.noreply.github.com>
2018-12-02 16:45:08 +0100

    JAVA SCRIPT  CW8 02.12.2018

--- main.js
+++ main.js
@@ -1,73 +1,90 @@
-const newP = document.createElement('p');
-const newPText = document.createTextNode('Przykładowy tekst');
-newP.appendChild(newPText);
-newP.setAttribute('id','test');
+/* let $test= document.querySelector('[data-test-array]');
+$test=$test.getAttribute('data-test-array');
 
-const parentListP = document.getElementById('parent-element');
-parentListP.appendChild(newP);
+$test= $test.split(' ');
+const test = document.getElementById('test');
+test.textContent = $test;
+console.log(test); */
 
-let tab=['a','b', 'c'];
-for (let i=0; i<tab.length; i++){
-    const newP= document.createElement('P');
-    const newPText = document.createTextNode(`Przykładowy ${1+tab[i]}`);
-    newP.appendChild(newPText);
-    newP.setAttribute('id',`test- ${i+1}`);
-    parentListP.appendChild(newP);
-}
 /*
-for (let i=0; i<3; i++){
+const btn=document.getElementById('test');
+btn.onclick= function(){
+    const $test = document.querySelector('[data-id]');
+    if (this.id === $test.getAttribute('data-id')){
+        $test.classList.remove('hide');
+    }
+}
+*/
+
+const obj = {};
+obj.test = 'tekst';
+obj.drugie = 'cos';
+console.log(obj);
+
+function testFn(paramString){ // function testFn(paramString='default'){
+    //sprawdza czy istnieje parametr, jezeli nie domyslna wartość
+    paramString=paramString || 'default'; 
     
-const newPText = document.createTextNode(`Przykładowy ${1+i}`);
-const newTexth2 = document.createTextNode(`${1+i}`); 
-//tworzenie nowego h2
-const newh2=document.createElement("h2");
-//wrzucanie tekstu do h2
-newh2=appendChild(newTexth2);
-parentListP.appendChild('newh2');
+const obj2={
+    start:123
+};
+ //ten if to samo co paramString=paramString || 'default';
+//if (paramString) { 
+   
+    obj2.abc = paramString; 
+//}
+//console.log(obj2);
+obj2.abc = paramString;
+return obj2;
 }
-//tworzenie nowego p
-const newh2=document.createElement("p");
-//wrzucanie tekstu do p
-newP=appendChild(newTextP);
-parentListP.appendChild('newP');
 
-/*
-newh2.onclick=function(){
-    if (newP.classList.contains('hide'){
-        newP.classList.add('newTextP');
-    }       
-           
-    else 
-    newP.classList.remove('newTextP');
-} */
-// biblioteka do obługi DOM umrellajs
+//testFn();
+const $testFn= testFn();
+console.log($testFn); // ($testFn.start)konkretny atrybut obiektu
+
+
+
 document.forms.myform.onsubmit= function() {
     if (this.nazwa.value){
-        newP=appendChild(this.nazwa.value);
-       parentListP.appendChild('newP');
-      //  var li=document.createElement("LI");
-      //  li.innerHTML=this.nazwa.value;
-       // document.getElementById("lista").appendChild(li);
-        this.nazwa.value= "";
+        {
+        var li=document.createElement("LI");
+       li.innerHTML=this.nazwa.value;
+       document.getElementById("ToDo").appendChild(li);
+       this.nazwa.value= "";
     this.nazwa.focus();
-      //  li.onclick=usuń;
+   //2
+    let ar=document.getElementsByTagName("li");
+   const newbtn=document.createElement('button');
+    const newbtnText = document.createTextNode("usun element");
+    li.appendChild(newbtn);
+    newbtn.appendChild(newbtnText);
+    for (i=0; i<ar.length; i++) {
+    newbtn.setAttribute('id',`usun ${i+1}`);
+    const $btn=document.getElementById(`usun ${i+1}`);
+  $btn.onclick= usuwanie;
     }
-    return false;
+  //3
+  const newbtn2=document.createElement('button');
+    const newbtn2Text2 = document.createTextNode("edycja elementu");
+    li.appendChild(newbtn2);
+    newbtn2.appendChild(newbtn2Text2);
+    for (i=0; i<ar.length; i++) {
+    newbtn2.setAttribute('id',`edycja ${i+1}`);
+    const $btn2=document.getElementById(`edycja ${i+1}`);
+    $btn2.onclick= edycja;
+   }
+    }
 }
 
-//const newa=document.createElement("a");
-//const newTexta = document.createTextNode("usun"); 
-//newa=appendChild(newTexta);
-//parentListP.appendChild(newa);
-//newa.onclick= function(){
-  //  newa.parentNode.removeChild(this);
-
-
-
-newP=onmouseout = function()
-{this.styleMedia.bacgraund = 'red';}
-
-  
-
-
-
+    
+    return false;
+}
+// funkcja do drugiego
+function usuwanie(){
+ this.parentNode.remove(this);
+}
+// funkcja do trzeciego
+function edycja(){
+    let ed=document.getElementsByTagName("li").contentEditable="true";
+//li.contentEditable=true;
+}
